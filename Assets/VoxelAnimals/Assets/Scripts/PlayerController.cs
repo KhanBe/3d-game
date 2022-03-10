f362491c084559464ce994406f8cdad8da4e4cff
WooJaeHak <wistm6980@gmail.com>
2022-03-10 23:00:17 +0900

    commit
    get anim, character, map basic

---
+++ Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 3;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
+    private float canJump = 0f;
+    Animator anim;
+    Rigidbody rb;
+
+    float moveHorizontal;
+    float moveVertical;
+
+    Vector3 movement;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ControllPlayer();
+    }
+
+    void ControllPlayer()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+        if (movement == Vector3.zero)
+        {
+            anim.SetInteger("Walk", 0);
+        }
+        else {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);// 몸체 회전
+            anim.SetInteger("Walk", 1);
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);//포지션 바꿔주기
+
+        if (Input.GetButtonDown("Jump") && Time.time > canJump)
+        {
+                rb.AddForce(0, jumpForce, 0);
+                canJump = Time.time + timeBeforeNextJump;
+                anim.SetTrigger("jump");
+        }
+    }
+}
