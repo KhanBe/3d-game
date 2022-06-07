bb910075c19db49d99a0187960628e701cbb9105
WooJaeHak <wistm6980@gmail.com>
2022-06-08 00:51:35 +0900

    player movement
    player movement good

--- Assets/Scripts/TPSCharacterController.cs
+++ Assets/Scripts/TPSCharacterController.cs
@@ -10,21 +10,65 @@ public class TPSCharacterController : MonoBehaviour
     private Transform cameraArm;
 
     Animator anim;
+    Rigidbody rb;
+
+    private float canJump = 0f;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
 
     void Start()
     {
         anim = characterBody.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         LookAround();
+        Move();
     }
+
+    void Move()
+    {
+        Vector3 moveDir = Vector3.zero;
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir;
+            transform.position += moveDir * Time.deltaTime * 5f;
+        }
+
+        if (moveDir == Vector3.zero) anim.SetInteger("Walk", 0);
+        else anim.SetInteger("Walk", 1);
+        //카메라암 방향 레이
+        //Debug.DrawRay(cameraArm.position, cameraArm.forward, Color.red);
+        //Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
+
+        if (Input.GetButtonDown("Jump") && Time.time > canJump)
+        {
+            rb.AddForce(0, jumpForce, 0);
+            canJump = Time.time + timeBeforeNextJump;
+            anim.SetTrigger("jump");
+        }
+    }
+
     void LookAround()//마우스로 카메라 회전하는 함수
     {
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         Vector3 camAngle = cameraArm.rotation.eulerAngles;
 
-        cameraArm.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y, camAngle.y + mouseDelta.x, camAngle.z);
+        float x = camAngle.x - mouseDelta.y;
+
+        //위로회전 제한
+        if (x < 180f) x = Mathf.Clamp(x, -1f, 70f);
+        //아래회전 제한
+        else x = Mathf.Clamp(x, 335f, 361f);
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
     }
 }
